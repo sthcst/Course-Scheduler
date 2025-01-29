9d5caad9889d46a0336aeb26f4be9384fc79d45a
icandari <isaaccandari@gmail.com>
2025-01-29 00:55:52 -1000

    host postgres on docker

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
-// db.js
-
 const { Pool } = require('pg');
 
 // Initialize PostgreSQL Connection Pool
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://username:password@localhost:5432/isaaccandari',
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:mysecretpassword@localhost:5432/postgres',
 });
 
 // Export the pool for use in other modules
