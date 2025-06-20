1d566444da56e9beb65263744e3f337efe7d8fbd
Seth Acosta <acostavseth@gmail.com>
2025-06-20 11:26:46 -1000

    Add "Courses" link on footer

--- public/js/footer.js
+++ public/js/footer.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <a href="https://byuh.edu" target="_blank" rel="noopener noreferrer">BYU-Hawaii</a>
                 <a href="https://holokai.m.byuh.edu/p/build-your-holokai" target="_blank" rel="noopener noreferrer">Build Your Holokai</a>
                 <a href="https://advising.byuh.edu/academic-advisors" target="_blank" rel="noopener noreferrer">Academic Advising</a>
+                <a href="https://catalog.byuh.edu/courses" target="_blank" rel="noopener noreferrer">Courses</a>
             </div>
             <p>&copy; ${new Date().getFullYear()} Brigham Young University-Hawaii. All rights reserved.</p>
         `;
