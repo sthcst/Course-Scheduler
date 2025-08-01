a42773a3118afd6fa8daa1753055fa7cdcc468e7
Seth <acostavseth@gmail.com>
2025-08-01 13:37:40 -1000

    fixed node

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios'); // Add this at the top with other imports
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 // Middleware to parse JSON bodies
