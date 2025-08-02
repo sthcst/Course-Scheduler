a8c9f271aa99b6454206586977ce95f82ff22510
Seth <acostavseth@gmail.com>
2025-08-01 15:07:28 -1000

    add another line

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg'); // Add the missing import
-
+const isProduction = process.env.NODE_ENV === 'production';
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: isProduction
