994f85b2e8b0dce399bac6b40ec62c784c498d31
Seth <acostavseth@gmail.com>
2025-08-04 11:57:38 -1000

    a

--- routes/api.js
+++ routes/api.js
@@ -2,20 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./db');
 
-// Optional: DB connection test
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack);
-  }
-  client.query('SELECT NOW()', (err, result) => {
-    release();
-    if (err) {
-      return console.error('Error executing query', err.stack);
-    }
-    console.log('Database connected:', result.rows);
-  });
-});
-
 module.exports = pool;
 
 /**
